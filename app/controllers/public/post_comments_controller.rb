dc4c36d9a93be4c2d67880023061dbf2fac34af0
ailovemoney <119666255+ailovemoney@users.noreply.github.com>
2023-03-14 15:36:55 +0000

    [ADD]非同期通信の実装

--- app/controllers/public/post_comments_controller.rb
+++ app/controllers/public/post_comments_controller.rb
@@ -1,17 +1,19 @@
 class Public::PostCommentsController < ApplicationController
   def create
-    post = Post.find(params[:post_id])
+    @post = Post.find(params[:post_id])
     # comment = PostComment.new(post_comment_params)
     # comment.user_id = current_user.id この２行を以下の一行に。
     comment = current_user.post_comments.new(post_comment_params)
-    comment.post_id = post.id
+    comment.post_id = @post.id
     comment.save
-    redirect_to post_path(post)
+    # redirect_to post_path(post) ←非同期通信のため削除
+    @post_comment = PostComment.new
   end
 
   def destroy
     PostComment.find(params[:id]).destroy
-    redirect_to post_path(params[:post_id])
+    # redirect_to post_path(params[:post_id]) ←非同期通信のため削除
+    @post = Post.find(params[:post_id])
   end
 
   private
