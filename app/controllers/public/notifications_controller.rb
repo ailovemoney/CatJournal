aafa72aef7db6a25c27796a8594dd5202ee726b9
ailovemoney <119666255+ailovemoney@users.noreply.github.com>
2023-03-27 14:26:22 +0000

    [ADD]通知機能実装

--- app/controllers/public/notifications_controller.rb
+++ app/controllers/public/notifications_controller.rb
@@ -1,6 +1,6 @@
 class Public::NotificationsController < ApplicationController
   def index
-    @notifications = current_user.passive_notifications
+    @notifications = current_user.passive_notifications.page(params[:page])
     # 未確認の通知レコードだけを取り出した後、未確認から確認済に変更する
     @notifications.where(checked: false).each do |notification|
       notification.update(checked: true)
