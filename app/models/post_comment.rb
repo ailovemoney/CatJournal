a57726457520d5cd90b21461ad99f5b0cc65b628
ailovemoney <119666255+ailovemoney@users.noreply.github.com>
2023-05-08 13:06:31 +0000

    [ADD]コメント機能の空欄禁止

--- app/models/post_comment.rb
+++ app/models/post_comment.rb
@@ -2,4 +2,7 @@ class PostComment < ApplicationRecord
   belongs_to :user
   belongs_to :post
   has_many :notifications, dependent: :destroy
+  
+  # 投稿する際に、空欄を禁止する。
+  validates :comment, presence: true
 end
